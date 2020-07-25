90895887fac1363f846e4be6d9596eda0c150ead
phatdroid94 <phatdroid94@gmail.com>
2020-07-25 19:07:02 +0700

    pending intent

--- app/src/main/java/com/example/notification02062020/MainActivity.java
+++ app/src/main/java/com/example/notification02062020/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.notification02062020;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -25,6 +29,18 @@ public class MainActivity extends AppCompatActivity {
         mBtnCreateNotifi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                intent.putExtra("chuoi","Restart Activity");
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(
+                                MainActivity.this,
+                                123,
+                                intent,
+                                PendingIntent.FLAG_UPDATE_CURRENT);
+
                 NotificationManager notificationManager =
                         (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 NotificationCompat.Builder builder =
@@ -38,7 +54,8 @@ public class MainActivity extends AppCompatActivity {
                                         R.drawable.ic_baseline_mood_24)
                         )
                         .setContentTitle("Thông báo có nội dung mới")
-                        .setContentText("Sự kiện sắp được ra mắt");
+                        .setContentText("Sự kiện sắp được ra mắt")
+                        .setContentIntent(pendingIntent);
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
                     NotificationChannel notificationChannel =
                             new NotificationChannel(
@@ -49,9 +66,8 @@ public class MainActivity extends AppCompatActivity {
                     notificationManager.createNotificationChannel(notificationChannel);
                 }
                 notificationManager.notify(1,builder.build());
-
-
             }
         });
     }
+
 }
